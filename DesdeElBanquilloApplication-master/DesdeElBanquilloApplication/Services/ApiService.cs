2fa25b55ea3ea9762848997c7966db47296fd32a
jhonatanTipan <jhonatan.tipan@udla.edu.ec>
2025-07-14 20:33:20 -0500

    Push prueba

---
+++ DesdeElBanquilloApplication-master/DesdeElBanquilloApplication/Services/ApiService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace DesdeElBanquilloApplication.Services
+{
+    public class ApiService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<T> GetFromApiAsync<T>(string url)
+        {
+            try
+            {
+                var response = await client.GetStringAsync(url);
+                return JsonConvert.DeserializeObject<T>(response);
+            }
+            catch (HttpRequestException e)
+            {
+                // Maneja errores de conexión
+                Console.WriteLine($"Request error: {e.Message}");
+                return default(T);
+            }
+        }
+
+
+
+    }
+}
+
